60bedca030f8b342503b43e57fea9b97c9ba0e0c
Rafal <rafal.grzesik.dev@gmail.com>
2022-01-11 12:05:43 +0100

    added all files

---
+++ src/js/glide.js
@@ -0,0 +1,20 @@
+
+new Glide('.glide', {
+    type: 'carousel',
+    autoplay: 3000,
+    perView: 3,
+    gap: 30,
+    animationDuration: 1000,
+    breakpoints: {
+        540: {
+            perView: 1
+        },
+        790: {
+            perView: 2
+        },
+        992: {
+            perView: 3
+        }
+    }
+
+}).mount()
